79a46033321874c105cc804db2f470c49311f481
wilsoncoronel <wilson_coronel_benitez@hotmail.com>
2025-09-29 21:45:28 -0500

    Actualizacion busqueda de articulos

--- obj/Debug/net9.0-windows/TiendaLaLojanita.AssemblyInfo.cs
+++ obj/Debug/net9.0-windows/TiendaLaLojanita.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TiendaLaLojanita")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c8082d45175503b898edbbd5c70e8774677fc82e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4c73c6d4fe09458aea362f32fbed02ae79556480")]
 [assembly: System.Reflection.AssemblyProductAttribute("TiendaLaLojanita")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TiendaLaLojanita")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
